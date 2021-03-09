d2ebcbf7bcd84a273c7448e751f6506b85b39c19
Aayush Gour <48613499+AayushGour@users.noreply.github.com>
2021-03-09 10:34:16 +0530

    Delete synk-music directory

--- synk-music/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import reducer from "./reducer"
-import { initialState } from "./initialState"
-import { createStore } from "redux"
-
-let store = createStore(reducer, initialState)
-export default store;
